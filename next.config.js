df0d6803a2331e7b87d909b703c039cf8ae53d3c
Onur Bozkurt <onurbozkurt@Mac.lan>
2025-03-18 10:47:29 -0700

    config: Next.js yapılandırması güncellendi

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
   webpack: (config) => {
